0c670ca9a35b9d2d06f13b8e9cf45206aead0383
Martin-J-Larre <martinjlarre@gmail.com>
2023-06-05 00:07:07 -0400

    #10 init server with node and express

---
+++ server/index.js
@@ -0,0 +1,13 @@
+const express = require("express");
+require("dotenv").config();
+
+const app = express();
+const PORT = process.env.PORT || 5000;
+
+app.get("/", (req, res) => {
+  res.send("This is working ok");
+});
+
+app.listen(PORT, () => {
+  console.log(`Server running on port http://localhost:${PORT}`);
+});
