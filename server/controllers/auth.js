18d47517352f81178786ce9a5fe02a5a0f87b301
Martin-J-Larre <martinjlarre@gmail.com>
2023-06-06 18:08:43 -0400

    #14 UserModel created, email validated and saved on db

--- server/controllers/auth.js
+++ server/controllers/auth.js
@@ -1,23 +1,43 @@
-exports.createUser = (req, res) => {
-  const { name, email, password } = req.body;
+const User = require("../models/user");
 
-  res.status(201).json({
-    ok: "ok",
-    message: "createUser",
-    name,
-    email,
-    password,
-  });
+exports.createUser = async (req, res) => {
+  const { email } = req.body;
+  try {
+    let userEmail = await User.findOne({ email });
+
+    if (userEmail) {
+      return res.status(400).json({
+        ok: false,
+        message: "Email already exist",
+      });
+    }
+
+    const user = new User(req.body);
+    const userSaved = await user.save();
+
+    res.status(201).json({
+      ok: true,
+      message: "User saved",
+      uid: userSaved.id,
+      name: userSaved.name,
+    });
+  } catch (error) {
+    console.log(error);
+    res.status(500).json({
+      ok: false,
+      message: "Error trying to save User",
+    });
+  }
 };
 
 exports.loginUser = (req, res) => {
   const { email, password } = req.body;
 
   res.status(200).json({
-    ok: "ok",
-    message: "loginUser",
-    email,
-    password,
+    // ok: true,
+    // message: "User saved",
+    // uid: userSaved.id,
+    // name: userSaved.name,
   });
 };
 
