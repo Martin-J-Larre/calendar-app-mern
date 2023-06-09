b79d91ac91d5cc7baf00a5250727e1e5ca1b84e3
Martin-J-Larre <martinjlarre@gmail.com>
2023-06-08 22:07:59 -0400

    #19 relative path User error fixed for deploy

--- server/index.js
+++ server/index.js
@@ -6,7 +6,6 @@ const { dbConnection } = require("./db/config");
 
 const app = express();
 dbConnection();
-const PORT = process.env.PORT || 5000;
 
 app.use(cors());
 app.use(express.json());
@@ -14,6 +13,6 @@ app.use(express.json());
 app.use("/api/auth", require("./routes/auth"));
 app.use("/api/event", require("./routes/event"));
 
-app.listen(PORT, () => {
-  console.log(`Server running on port http://localhost:${PORT}`);
+app.listen(process.env.PORT, () => {
+  console.log(`Server running on port http://localhost:${process.env.PORT}`);
 });
